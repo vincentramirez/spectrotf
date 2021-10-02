3cc27cd423cc7367dfd71a4a80d8813b2b27f4b4
vincentramirez <vinnie.ramirez@gmail.com>
2021-10-02 05:12:08 -0700

    fix vars.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,9 @@ description = "Spectro Cloud API endpoint"
   type        = string
   default     = "api.spectrocloud.com" # for enterprise mode," private-hosted", enter DNS name
 }
-variable "sc_username" = {} #e.g: user1@abc.com
-variable "sc_password" = {} #e.g: superSecure1!
-variable "sc_project_name" = {     
+variable "sc_username" {} #e.g: user1@abc.com
+variable "sc_password" {} #e.g: superSecure1!
+variable "sc_project_name" {     
 description = "Spectro Cloud Project name" 
   type        = string
   default     = "Default"  #e.g.: use "Default" if no projects have been created
